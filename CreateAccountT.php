1995b3cfe596921c8b0b52a17cf60a5bc12b3f89
rogueflynn <vgvgonzalez8@gmail.com>
2016-09-12 20:34:27 -0400

    PHP Code

---
+++ CreateAccountT.php
@@ -0,0 +1,36 @@
+<?php
+	//Email and password
+	$Email = $_REQUEST["Email"]; 
+	$Password = $_REQUEST["Password"]; 
+
+	//PHP Only
+	$Hostname = "localhost";
+	$DBName = "gameaccounts";
+	$User = "vic";
+	$PasswordP = "emily";
+
+	$conn = new mysqli($Hostname, $User, $PasswordP, $DBName);
+	
+	if($conn->connect_error) {
+		die("Connection failed: " . $conn->conect_error);
+	}
+
+	if(!$Email || !$Password) {
+		echo "Empty";
+	} else {
+		$SQL = "SELECT * FROM  accounts where Email ='" . $Email . "'";
+		$Result = mysqli_query($conn, $SQL);
+		$Total = mysqli_num_rows($Result);
+		if($Total == 0) {
+			$insert = "INSERT INTO accounts (Email, Password) values ('" . $Email . "', MD5('" . $Password . "'))";
+			$SQL1 = mysqli_query($conn, $insert);
+			echo "Success";
+		} else {
+			echo "AlreadyUsed";
+		}
+
+	}
+
+	//close mysql
+	mysqli_close($conn);
+?>
